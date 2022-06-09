36c69bb69470dc42f52bbb2049d555779ac82f0e
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-06-09 13:48:08 -0400

    ketchup

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const server = require()
+require('dotenv').config();
+
+const server = require('./api/server');
+
+console.log('process.env.PORT:', process.env.PORT);
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+
 
 // // this function expects to receive either a 1 or a 2
 // function someProcedure(a) {
